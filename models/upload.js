301a7b01f260fd24ca4fc99525bb2b4117aee306
tinnamrichter <tinn0229@easv365.dk>
2020-05-01 12:47:25 +0200

    større billeder

--- models/upload.js
+++ models/upload.js
@@ -13,7 +13,7 @@ const storageEngine = multer.diskStorage({
 //init
 const upload =  multer({
   storage: storageEngine,
-  limits: { fileSize:200000 },
+  limits: { fileSize:2000000 },
   fileFilter: function(req, file, callback){
     validateFile(file, callback);
   }
