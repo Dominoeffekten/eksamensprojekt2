7ab4ab18d82782ff5e4cee5227e9b45120046699
Anders <anders1603@hotmail.com>
2020-05-03 15:24:38 +0200

    reklamebilleder

---
+++ public/javascripts/commercials.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { $ } from "./modules/nQuery.js";
+
+let commercial = [
+    'dplay.png',
+    'dplay1.png',
+    'dplay2.png',
+    'edc.png',
+    'harald.png',
+    'smarteyes.png',
+    'toyota.png',
+    'toyota1.png'
+];
+
+let changeCommercial = function () {
+
+    let randomCommercial = Math.floor(Math.random() * commercial.length);
+    $('reklame').src = `images/commercial/${commercial[randomCommercial]}`;
+    console.log(`Reklamen der sættes ind er: ${commercial[randomCommercial]}`)
+};
+
+window.addEventListener("load", changeCommercial);
