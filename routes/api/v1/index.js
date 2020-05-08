450ecacb82ec8ad23e54afeaa4d54bc377d501ce
tinnamrichter <tinn0229@easv365.dk>
2020-05-08 18:03:28 +0200

    nu er der en knap der ikke virker endnu

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -31,4 +31,6 @@ router.post("/v1/follow", function(req, res, next) {
     });
 });
 
+for(var i=0;i<user.followers.length;i++) { if(user.followers[i] == userId) { disabled }}
+
 module.exports = router;
