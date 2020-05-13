5ba8d3879d7e27b738140fcc1667aa363045c50c
Dominoeffekten <rikkedomino@gmail.com>
2020-05-13 08:36:51 +0200

    Oprydning

--- public/javascripts/darkTheme.js
+++ public/javascripts/darkTheme.js
@@ -42,26 +42,6 @@ const showTheme = function (e) {
         }
     }
     toggleSwitch.addEventListener("change", changeTheme);
-    
-/*
-    function switchTheme(e){
-        if(e.target.checked){
-            document.documentElement.setAttribute("data-theme", "dark");
-            localStorage.setItem("theme", "dark");
-        } else {
-            document.documentElement.setAttribute("data-theme", "light");
-            localStorage.setItem("theme", "light");
-        }
-    }
-
-    if (currentTheme) {
-        document.documentElement.setAttribute("data-theme", currentTheme);
-        if (currentTheme === "dark"){
-            toggleSwitch.checked = true;
-        }
-    }
-    toggleSwitch.addEventListener("change", switchTheme, false);
-*/
 };
 
 window.addEventListener("load", getTheme);
